bf3a2ba24ecb8fc0d67125e229e1ad828218a267
U-ENDLAB\arifimm <arifimm@END-ArifiMM.ENDLAB.com>
2017-08-20 11:39:55 +0300

    get plot to work

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ OUT_FILE = "temp"
 print("Python version to practice")
 import sys
 import time
+import matplotlib.pyplot as plt
+import numpy as np
 
 out_sec = []
 in_sec = []
+dayCnt = 0
 try:
     with open(IN_FILE) as f:
         for line in f:
             if line[0] != 'W':
+                dayCnt += 1
                 line_sp = line.split()
                 if len( line_sp ) > 2 :
                     day, inTime, outTime = line_sp
@@ -23,8 +27,18 @@ try:
                     outTime = None
                     inTime_sp = time.strptime(inTime.split()[0],'%H:%M:%S')
                     in_sec.append (inTime_sp.tm_hour*3600 + inTime_sp.tm_min*60 + inTime_sp.tm_sec)
-    print(in_sec)
-    print(out_sec)
 except IOError:
     print("Cannot open file")
     sys.exit(1)
+
+for i, val in enumerate(in_sec):
+    if val == 0:
+        in_sec[i] = np.nan
+
+x_axis = range(dayCnt)
+
+print(in_sec)
+print(out_sec)
+#plt.plot(x_axis, in_sec)
+plt.scatter(x_axis, in_sec)
+plt.show()
